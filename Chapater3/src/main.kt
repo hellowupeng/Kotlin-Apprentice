6f9f608c4f506781c9858c02d343798d340eb1f8
Andy Wu <hellowupeng@gmail.com>
2018-05-04 07:34:18 +0800

    .

---
+++ Chapater3/src/main.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>): Unit {
+//    var integer: Int = 100
+//    var decimal: Double = 12.5
+//    integer = decimal.toInt()
+//    println(integer)
+
+//    var hourlyRate: Double = 19.5
+//    val hoursWorked: Int = 10
+//    val totalCost: Double = hourlyRate * hoursWorked
+//    println(totalCost)
+
+//    val typeInferredInt = 42
+//    val typeInferredDouble = 3.14159
+//
+//    val wantADouble = 3
+//    val actuallyDouble = 3.toDouble()
+
+//    val age1 = 42
+//    val age2 = 21
+//
+//    val avg1 = (age1.toDouble() + age2.toDouble()) / 2
+//    println(avg1)
+
+//    val characterA: Char = 'a'
+//    val stringDog: String = "Dog"
+//    val stringDog = "Dog" // Infered to be of type String
+//    println(characterA)
+//    println(stringDog)
+
+    var message = "Hello" + " my name is "
+    val name = "Dick"
+    message += name // "Hello my name is Dick"
+    println(message)
+}
