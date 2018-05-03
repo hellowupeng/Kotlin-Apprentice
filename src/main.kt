e1a7df526d705cfc7da3a24ca29f6fec2fd9689d
Andy Wu <hellowupeng@gmail.com>
2018-05-03 22:57:16 +0800

    .

---
+++ src/main.kt
@@ -0,0 +1,57 @@
+import kotlin.math.*
+
+fun main(args: Array<String>): Unit {
+//    println("Hello, Kotlin Apprentice reader!")
+
+//    println(2 + 6)
+//    println(10 - 2)
+//    println(2 * 4)
+//    println(24 / 3)
+
+//    println("%.0f".format(28.0 % 10.0))
+
+//    println(1 shl 3)
+//    println(32 shr 2)
+
+//    println(sin(45 * PI / 180))
+//    println(cos(135 * PI / 180))
+
+//    println(sqrt(2.0))
+//    println(max(5, 10))
+//    println(min(-5, -10))
+//    println(max(sqrt(2.0), PI / 2))
+
+//    val number: Int = 10
+//    val pi: Double = 3.14159
+//
+//    var variableNumber: Int = 42
+//
+//    variableNumber = 0
+//    variableNumber = 1_000_000
+
+//    var counter: Int = 0
+//
+//    counter += 1
+//    // counter = 1
+//
+//    counter -= 1
+//    println("counter: $counter")
+
+//    var counter: Int = 0
+//    counter = counter + 1
+//    counter = counter - 1
+
+//    counter = 10
+//    counter *= 3 // same as counter = counter * 3
+//    // counter = 30
+//
+//    counter /= 2 // same as counter = counter / 2
+//    // counter = 15
+
+//    val myAge: Int = 24
+//    var averageAge: Double = 24.0
+//    averageAge = 27.0
+
+    val exercises = 9
+    var exercisesSolved = 0
+}
